391f046257ee0448deda8b16c7f3ab89018c51b1
Jiri S <jirka@chlivek.com>
2017-12-05 20:48:24 +0100

    Create xmr2.sh

---
+++ xmr2.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+printf "monero\nxmrpool.eu:3333\nSumoo5HXHPrKd9sdKkvXyUTfdYfgdwCd6VCq4Hr6gGkye7gwQXj3g7x8TUhMFhHxKi2Z4Xv72PZqRT6NrD44tnB3bwqqoFJ46n2\ndck\nn\nn\nn\n" | /opt/xmr-stak/build/bin/xmr-stak
