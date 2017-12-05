64b15389298f253455e2dc2517141591b38dce2d
Jiri S <jirka@chlivek.com>
2017-12-05 16:17:42 +0100

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-printf "monero\nsumo.bohemianpool.com:6666\nSumoo5HXHPrKd9sdKkvXyUTfdYfgdwCd6VCq4Hr6gGkye7gwQXj3g7x8TUhMFhHxKi2Z4Xv72PZqRT6NrD44tnB3bwqqoFJ46n2\ndck\n\n\nn\nn\n" | /opt/xmr-stak/build/bin/xmr-stak
+printf "monero\nsumo.bohemianpool.com:6666\nSumoo5HXHPrKd9sdKkvXyUTfdYfgdwCd6VCq4Hr6gGkye7gwQXj3g7x8TUhMFhHxKi2Z4Xv72PZqRT6NrD44tnB3bwqqoFJ46n2\ndck\nn\nn\nn\n" | /opt/xmr-stak/build/bin/xmr-stak
