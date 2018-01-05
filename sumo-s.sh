104fd1adb87c892d7fbe72ad069bf220d6e46ea3
Jiri S <jirka@chlivek.com>
2018-01-05 13:43:36 +0100

    Create sumo-s.sh

---
+++ sumo-s.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+printf "monero\nsumo.kryptopond.net:3333\nSumoo6Tk19oFWeUrDon3XTRhAPtZtEFcBXtF33mznZL3VykBZEWT5ZxV9z1v3hfJEUbQidV7H55ox1v5coUk79pNTgsbZo19P8s+dck\nx\nn\nn\nn\n" | /opt/xmr-stak/build/bin/xmr-stak
