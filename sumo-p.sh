5a698f3dc2947f2bf9b506d0614b7a59b401feea
Jiri S <jirka@chlivek.com>
2024-01-22 14:53:36 +0100

    Delete sumo-p.sh

--- sumo-p.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-printf "monero\nsumo.kryptopond.net:3333\nSumoo6boDhVUo4S7uaR3GeKPyACmfUzy4ZJoCKM6TZHBJq7N5SX48AxAf5GCAYEqkvTeiA8i5TaTqFS7JJskkGksEqLDKj1gUU8+dck\nx\nn\nn\nn\n" | /opt/xmr-stak/build/bin/xmr-stak
