cd60c92b2534d278c94897831ca3ff350247b741
Jiri S <jirka@chlivek.com>
2024-01-22 14:53:49 +0100

    Delete xmr.sh

--- xmr.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-printf "monero\nsumo.bohemianpool.com:6666\nSumoo5HXHPrKd9sdKkvXyUTfdYfgdwCd6VCq4Hr6gGkye7gwQXj3g7x8TUhMFhHxKi2Z4Xv72PZqRT6NrD44tnB3bwqqoFJ46n2\ndck\nn\nn\nn\n" | /opt/xmr-stak/build/bin/xmr-stak
-
