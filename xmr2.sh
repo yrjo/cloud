30159b2e9bcccd6f78a46fa5aab26cea6da11c9d
Jiri S <jirka@chlivek.com>
2024-01-22 14:53:59 +0100

    Delete xmr2.sh

--- xmr2.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-printf "monero\nxmrpool.eu:3333\n4AXQcDDsLTtLdSAUTGYs5ZhrQkc8HkoeVfBDw56LvzNyQagog5D6u2ZPX1C89ECodwbQ9FoZMznn28SJWXKe3AmT9wVQTDu.20000+dck\nx\nn\nn\nn\n" | /opt/xmr-stak/build/bin/xmr-stak
